7610a4ca3d0f0ae24fdc37d600b2f900edd90708
vivien <vivien34400@laposte.net>
2016-10-24 18:57:33 +0200

    Implementation security

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -8,11 +8,47 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
 /**
  * Description of LoginController
  *
  * @author hugues
  */
-class LoginController {
-    //put your code here
+class LoginController extends Controller {
+    
+    /**
+     * @Route("/loginCheck",name="loginCheck")
+     * @throws Exception
+     */
+    public function check() {
+        throw new Exception('Verifiez votre fichier security');
+    }
+    /**
+     * idem déconnexion
+     * @Route("/loginOut",name="loginOut")
+     * @throws Exception
+     */
+    public function logout() {
+        throw new Exception('Verifiez votre fichier security');
+    }
+    /**
+     * @Route("/add",name="add")
+     * @throws Exception
+     */
+    public function add() {
+        $u = new User();
+        $u->setName("admin");
+        $u->setPass("admin");
+        $u->setRoles(array("ROLE_ADMIN"));
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($u);
+        $em->flush();
+        
+        return $this->redirectToRoute("home");
+    }
 }
